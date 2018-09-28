fa09c94649b14e861a966c8472503f05b393e446
Cproyi <2049500238@qq.com>
2018-09-28 12:34:07 +0800

    initial
    initial

---
+++ dpointLogin/src/main/java/com/dpointLogin/dpointLogin/DpointLoginApplication.java
@@ -0,0 +1,12 @@
+package com.dpointLogin.dpointLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpointLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpointLoginApplication.class, args);
+	}
+}
